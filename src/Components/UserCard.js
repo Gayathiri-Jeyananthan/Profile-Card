3bf766c07df790a527c37c267dcb6b4a80490a72
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-06-03 21:39:49 +0530

    Final commit

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,13 +1,40 @@
-import React from "react";
+const userData=[
+    {name:"Jeromi",
+    city:"Colombo",
+    work:"Front-end Developer",
+    skills:['Html','CSS','Bootstrap','JavaScript','TypeScript','React','MongoDB'],
+    online:false,
+    profile:"images/1.jpg"
+},
 
-function User() {
+{name:"Sonia",
+    city:"Jaffna",
+    work:"Back-end Developer",
+    skills:['Vue.js','Express','Node.js','JavaScript','TypeScript','React','MongoDB'],
+    online:true,
+    profile:"images/2.jpg"
+},
+
+{
+    name:"Alexa",
+    city:"Kandy",
+    work:"UI-UX Designer",
+    skills:['Html','CSS','Bootstrap','Tailwind.css','Material UI','Angular','Adobe'],
+    online:false,
+    profile:"images/3.jpg"
+}
+
+]
+
+
+function User(props) {
   return (
     <div className="card-container">
-      <span className="pro">ONLINE</span>
-      <img src="images/1.jpg" className="img" alt="user" />
-      <h3>User Full Name</h3>
-      <h3>Colombo</h3>
-      <p>Front-end Developer</p>
+      <span className={props.online?"pro online":"pro offline"}>{props.online?"ONLINE":"OFFLINE"}</span>
+      <img src={props.profile}className="img" alt="user" />
+      <h3>{props.name}</h3>
+      <h3>{props.city}</h3>
+      <p>{props.work}</p>
       <div className='buttons'>
         <button className="primary">Message</button>
         <button className="primary outline">Following</button>
@@ -15,18 +42,32 @@ function User() {
       <div className="skills">
         <h6>Skills</h6>
         <ul>
-          <li>Html</li>
-          <li>CSS</li>
-          <li>Bootstrap</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>React</li>
-          <li>MongoDB</li>
+          {props.skills.map((skills,index) => (
+            <li key={index}>{skills}</li>
+          ))}
         </ul>
       </div>
     </div>
   );
 }
 export const UserCard = () => {
-  return <User />;
+   return( <>
+   {
+    userData.map((user,index) => (
+        <User key={index} 
+        name={user.name}
+        city={user.city}
+        work={user.work}
+        skills={user.skills}
+        online={user.online}
+        profile={user.profile}
+
+        />
+        ))
+   }
+    </>
+    )
 };
+
+// return <User name="Jeromi" city="Colombo" work="Front-end Developer" skills={['Html','CSS','Bootstrap','JavaScript','TypeScript','React','MongoDB']} online={true} profile="images/1.jpg" />;
+
